3ce8389064d43f18139e35411ebb7d27e0b0eca6
flashpaykg <flashpay@ecommlabs.dev>
2024-08-30 00:38:54 +0300

    publish

---
+++ src/main/java/com/flashpay/sdk/PaymentPage.java
@@ -0,0 +1,57 @@
+package kg.flashpay.sdk;
+
+import java.io.UnsupportedEncodingException;
+import java.net.URLEncoder;
+import java.util.stream.Collectors;
+
+/**
+ * Class for build payment URL
+ */
+public class PaymentPage
+{
+    /**
+     * payment domain with path
+     */
+    private String baseUrl = "https://paymentpage.flashpay.kg/payment";
+
+    /**
+     * Signature handler for generate signature
+     */
+    private SignatureHandler signatureHandler;
+
+    /**
+     * kg.flashpay.sdk.PaymentPage constructor
+     * @param signHandler signature handler for generate signature
+     */
+    public PaymentPage(SignatureHandler signHandler) {
+        signatureHandler = signHandler;
+    }
+
+    /**
+     * Method for set base payment page URL
+     * @param url
+     * @return
+     */
+    public PaymentPage setBaseUrl(String url) {
+        baseUrl = url;
+
+        return this;
+    }
+
+    /**
+     * Method build payment URL
+     * @param payment kg.flashpay.sdk.Payment instance with payment params
+     * @return string URL that you can use for redirect on payment page
+     */
+    public String getUrl(Payment payment) {
+        payment.setParam("signature", signatureHandler.sign(payment.getParams()));
+
+        return
+            baseUrl
+                .concat("?")
+                .concat(payment.toString());
+
+    }
+
+
+}
